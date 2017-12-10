57601c6bf4bf9ae61904f8fe4eec83b9b932942d
Ma <tony@gmail.com>
2017-12-10 14:03:46 +0800

    修改程序图标和名称

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by Jay on 2017/12/7.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
